3eb73503056033c458445a5f7385f375f7116b10
Samrat Chakraborty <121852717+samratchakraborty422000@users.noreply.github.com>
2023-08-14 13:44:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+// Getting all the elements 
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
+let copyIcon = document.getElementById("copyIcon");
+
+
+// Showing the input slider value
+
+sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener('input', ()=>{
+    sliderValue.textContent = inputSlider.value;
+});
+
+// event for generating password
+
+genBtn.addEventListener('click', ()=>{
+    passBox.value = generatePassword();
+})
+
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allNumbers = "0123456789";
+let allSymbols = "~!@#$%^&*"; 
+
+// Function to generate the Password
+
+function generatePassword(){
+    let genPassword = "";
+    let allChars = "";
+    // after reviewing the check box adding the checked items 
+
+    allChars  += lowercase.checked ? lowerChars : "";
+    allChars  += uppercase.checked ? upperChars : "";
+    allChars  += numbers.checked ? allNumbers : "";
+    allChars  += symbols.checked ? allSymbols : "";
+
+    // When nothing is checked --Trivial Case
+
+    if(allChars == "" || allChars.length == 0){
+        return genPassword;
+    }
+    
+    // handling the non trivial cases
+    let i = 1;
+    while(i<=inputSlider.value){
+        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+        i++;
+    }
+
+    return genPassword;
+}
+
+// Setting up the copy icon
+
+copyIcon.addEventListener('click', ()=>{
+    if(passBox.value != "" || passBox.value.length >=1){
+        navigator.clipboard.writeText(passBox.value);
+        copyIcon.innerText = "check";
+        copyIcon.title = "Password Copied";
+
+        // Timer for visibility of the check sign
+
+        setTimeout(()=>{
+            copyIcon.innerHTML = "content_copy";
+            copyIcon.title = "";
+        }, 4000)
+
+    }
+    
+});
